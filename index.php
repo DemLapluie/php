92e7ac6142e7a21fa6972d74567073868039353e
Demmy ONDÉE <demmyondee@gmail.com>
2018-07-13 14:54:18 +0200

    changemnt accueil

--- index.php
+++ index.php
@@ -3,7 +3,8 @@ require_once __DIR__ . '/include/init.php';
 
 require __DIR__ . '/layout/top.php';
 ?>
-        <h1>Accueil</h1>
+        <h1>Page d'Accueil</h1>
+        <p>Bienvenue</p>
 <?php
 require __DIR__ . '/layout/bottom.php';
 ?>
